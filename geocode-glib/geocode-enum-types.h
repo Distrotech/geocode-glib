6220f9f1950a820e41e321c253a7773ca176b9c1
Greg Nietsky <gregory@distrotech.co.za>
2014-01-05 15:23:07 +0200

    Autotools

---
+++ geocode-glib/geocode-enum-types.h
@@ -0,0 +1,37 @@
+
+
+
+#ifndef __GEOCODE_ENUM_TYPES_H__
+#define __GEOCODE_ENUM_TYPES_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+/* enumerations from "geocode-error.h" */
+GType geocode_error_get_type (void) G_GNUC_CONST;
+#define GEOCODE_TYPE_ERROR (geocode_error_get_type())
+
+/**
+ * SECTION:geocode-enum-types
+ * @short_description: Geocode enumerated types
+ * @include: geocode-glib/geocode-glib.h
+ *
+ * The enumerated types defined and used by geocode-glib.
+ **/
+/* enumerations from "geocode-place.h" */
+GType geocode_place_type_get_type (void) G_GNUC_CONST;
+#define GEOCODE_TYPE_PLACE_TYPE (geocode_place_type_get_type())
+
+/**
+ * SECTION:geocode-enum-types
+ * @short_description: Geocode enumerated types
+ * @include: geocode-glib/geocode-glib.h
+ *
+ * The enumerated types defined and used by geocode-glib.
+ **/
+G_END_DECLS
+
+#endif /* __GEOCODE_ENUM_TYPES_H__ */
+
+
+
